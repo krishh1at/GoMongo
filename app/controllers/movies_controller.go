ddd38cf5344c907f8b8a67c5cbe0f73ad7189d7d
Krishna Pandit <krishna.pandit@bitcanny.com>
2022-03-27 20:45:48 +0530

    GetMovie replaced by findMovie

--- app/controllers/movies_controller.go
+++ app/controllers/movies_controller.go
@@ -9,7 +9,7 @@ import (
 	"github.com/krishh1at/app/models"
 )
 
-// Controllers routers methods
+// Get All movies list API
 func GetMovies(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
@@ -17,11 +17,13 @@ func GetMovies(w http.ResponseWriter, r *http.Request) {
 	helpers.RenderJson(w, result, err)
 }
 
-func ShowMovie(w http.ResponseWriter, r *http.Request) {
-	result := GetMovie(w, r)
+// Movie show API
+func GetMovie(w http.ResponseWriter, r *http.Request) {
+	result := findMovie(w, r)
 	helpers.RenderJson(w, result, nil)
 }
 
+// Create Movie API
 func CreateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Allow-Control-Allow-Methods", "POST")
 
@@ -32,32 +34,36 @@ func CreateMovie(w http.ResponseWriter, r *http.Request) {
 	helpers.RenderJson(w, result, err)
 }
 
+// Update Movie API
 func UpdateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
 
-	movie := GetMovie(w, r)
+	movie := findMovie(w, r)
 	_ = json.NewDecoder(r.Body).Decode(movie)
 
 	result, err := models.Update(movie)
 	helpers.RenderJson(w, result, err)
 }
 
+// Marked Watched Movie API
 func MarkWatchedMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
 
-	movie := GetMovie(w, r)
+	movie := findMovie(w, r)
 	result, err := movie.MarkedWatched()
 	helpers.RenderJson(w, result, err)
 }
 
+// Delete Movie API
 func DestroyMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
 
-	movie := GetMovie(w, r)
+	movie := findMovie(w, r)
 	result, err := models.Destroy(movie)
 	helpers.RenderJson(w, result, err)
 }
 
+// Delete All Movie API
 func DeleteAllMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
 
@@ -65,7 +71,8 @@ func DeleteAllMovie(w http.ResponseWriter, r *http.Request) {
 	helpers.RenderJson(w, result, err)
 }
 
-func GetMovie(w http.ResponseWriter, r *http.Request) *models.Movie {
+//
+func findMovie(w http.ResponseWriter, r *http.Request) *models.Movie {
 	params := mux.Vars(r)
 	result, err := models.Find(&models.Movie{}, params["id"])
 	if err != nil {
